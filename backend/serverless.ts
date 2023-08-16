1d051a4a419a4b2796f0a0f838deeb3bb59d830b
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-16 20:10:04 +0300

    works(for now)

--- backend/serverless.ts
+++ backend/serverless.ts
@@ -22,16 +22,15 @@ module.exports = serverless({
     }
   },
   custom: {
-    package: {
-      patterns: [
-        '!node_modules/.prisma/client/libquery_engine-*',
-        'node_modules/.prisma/client/libquery_engine-rhel-*',
-        '!node_modules/prisma/libquery_engine-*',
-        '!node_modules/@prisma/engines/**',
-        'node_modules/.prisma/client/schema.prisma'
-  
-      ]
-    }
+    // package: {
+    //   patterns: [
+    //     '!node_modules/.prisma/client/libquery_engine-*',
+    //     'node_modules/.prisma/client/libquery_engine-rhel-*',
+    //     '!node_modules/prisma/libquery_engine-*',
+    //     '!node_modules/@prisma/engines/**',
+    //     'node_modules/.prisma/client/schema.prisma'
+    //   ]
+    // }
   },
   functions: f,
   resources: r,
--- backend/src/ingredients/createIngredient/handler.ts
+++ backend/src/ingredients/createIngredient/handler.ts
@@ -6,35 +6,81 @@ const prisma = new PrismaClient();
 export default middyfy(async () => {
   try {
     // await prisma.ingredient.deleteMany()
-    
-    const ingredient = await prisma.ingredient.create({ data: {
-      name: 'White Rice',
-      description: 'Basmati white rice',
-      price: 0.027,
-      calories: 0.5,
-      protein: 0.3,
-      fats: 0.2,
-      carbs: 0.4,
-      unit: 'g'
-    }})
-    console.log(ingredient)
+
+    const ingredient = await prisma.ingredient.create({
+      data: {
+        name: 'cool af White Rice',
+        description: 'more rice',
+        price: 0.0227,
+        calories: 0.25,
+        protein: 0.23,
+        fats: 0.22,
+        carbs: 0.42,
+        unit: 'g',
+      },
+    });
+
+    console.log(ingredient);
+
     return {
       statusCode: 201,
       body: {
         message: 'Ingredient Created',
-        ingredient
-      }
-    }
+        ingredient,
+      },
+    };
   } catch (err) {
-    console.log(err)
+    console.log(err);
+
     return {
+      statusCode: 500,
       body: {
         error: {
           title: 'Prisma Error',
           message: 'Error creating ingredient',
-          err
-        }
-      }
-    }
+          details: err.message,
+        },
+      },
+    };
   }
 });
+
+
+
+// import { PrismaClient } from '@prisma/client';
+// import { middyfy } from '@lib/middleware';
+
+// const prisma = new PrismaClient();
+
+// export default middyfy(async (event) => {
+//   try {
+//     const ingredientData = JSON.parse(event.body);
+
+//     const ingredient = await prisma.ingredient.create({
+//       data: ingredientData,
+//     });
+
+//     console.log(ingredient);
+
+//     return {
+//       statusCode: 201,
+//       body: {
+//         message: 'Ingredient Created',
+//         ingredient,
+//       },
+//     };
+//   } catch (err) {
+//     console.log(err);
+
+//     return {
+//       statusCode: 500,
+//       body: {
+//         error: {
+//           title: 'Prisma Error',
+//           message: 'Error creating ingredient',
+//           details: err.message,
+//         },
+//       },
+//     };
+//   }
+// });
