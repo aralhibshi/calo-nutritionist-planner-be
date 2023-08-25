05440437ba8d06f38d9a8c1fe7c240a2cb6d3cd0
aralhibshi <118065642+aralhibshi@users.noreply.github.com>
2023-08-25 17:37:03 +0300

    feat: Singleton Meal Repository & Prisma Client Instance
    - Implement singleton repo instance for mealRepository
    - Replace separate repo instances in createMeal useCase with single repo instance
    - Remove "create" keyword from meal create route
    - Implement validation and exception middleware in createMeal Handler
    - Move string/data manipulation from createMeal Handler to useCase

--- src/services/meals/createMeal/handler.ts
+++ src/services/meals/createMeal/handler.ts
@@ -1,64 +1,29 @@
 import Joi from 'joi';
-import { PrismaClient, Prisma } from '@prisma/client';
 import { middyfy } from '@lib/middleware/eventParserMiddleware';
-import { createMeal } from './useCase';
-import { createMealComponent } from './useCase';
-import { capitalizeFirstLetter } from 'src/utils/stringUtils';
-import createError from 'http-errors';
-
-const prisma = new PrismaClient();
+import { bodyValidationMiddleware } from '@lib/middleware/validationMiddleware';
+import { readExceptionHandlerMiddleware } from '@lib/middleware/exceptionHandlerMiddleware';
+import { createMeal, createMealComponent } from './useCase';
 
 export default middyfy(async (event) => {
-  try {
-    console.log('Received CloudFormation Event:', JSON.stringify(event, null, 2));
+  console.log('Received CloudFormation Event:', JSON.stringify(event, null, 2));
 
-    // Joi Validation Schema
-    const validationSchema = Joi.object({
-      name: Joi.string().required(),
-      size: Joi.string().required(),
-      unit: Joi.string().required(),
-      components: Joi.array()
-      .min(1)
-      .required()
-    })
+  const validationSchema = Joi.object({
+    name: Joi.string().required(),
+    size: Joi.string().required(),
+    unit: Joi.string().required(),
+    components: Joi.array()
+    .min(1)
+    .required()
+  })
 
-    // Asynchronous Validation
-    try {
-      await validationSchema.validateAsync(event.body);
-    } catch (validationError) {
-      throw createError(400, 'Validation Error', {
-        details: validationError.details.map(detail => detail.message),
-      });
-    }
+  // Validation before Processing
+  bodyValidationMiddleware(validationSchema)(event);
 
-    const mealData = {
-      ...event.body,
-      name: capitalizeFirstLetter(event.body.name),
-      size: capitalizeFirstLetter(event.body.size),
-    };
-    const components = mealData.components;
-    delete mealData.components;
+  // useCase - Create Meal
+  const meal = await createMeal(event);
 
-    // useCase - Create Meal
-    const result = await createMeal(prisma, mealData);
-
-    // useCase - Create Meal Component
-    const parsedResult = JSON.parse(result.body)
-    const mealId = parsedResult.data.id;
-    
-    await createMealComponent(prisma, mealId, components)
-
-    return result;
-  } catch (err) {
-    console.log('Error:', err);
-    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
-      console.log('Conflict Error:', err);
-      throw createError(409, 'Conflict Error', {
-        details: err,
-      });
-    }
-    throw createError(500, 'Internal Server Error', {
-      details: 'An error occurred while creating the meal',
-    });
-  }
-});
+  // useCase - Create Meal Component
+  await createMealComponent(meal, event)
+  return meal;
+})
+.use(readExceptionHandlerMiddleware());
