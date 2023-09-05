5777bb73d7a0e1585e20658eb4abac2450d49ab8
aralhibshi <118065642+aralhibshi@users.noreply.github.com>
2023-09-05 16:28:05 +0300

    fix: CORS Testing

--- src/services/components/getComponents/useCase.ts
+++ src/services/components/getComponents/useCase.ts
@@ -9,12 +9,12 @@ export async function getComponents(
 
   const skip = Number(data.skip);
   const take = Number(data.take);
-  let name = 'Hello';
+  let name = data.name;
   const ingredientId = data.ingredient_id;
 
-  // if (name) {
-  //   name = capitalizeFirstLetter(name)
-  // }
+  if (name) {
+    name = capitalizeFirstLetter(name)
+  }
 
   // Repo - Get Components
   const result = await componentRepo.getComponents(skip, take, name, ingredientId);
